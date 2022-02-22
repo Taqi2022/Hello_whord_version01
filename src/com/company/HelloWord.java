e4b88e69a7c1ab52e77f17d0351f9148292f316b
Taqi20222 <Takibouzerzour42@gmail.com>
2022-02-22 15:56:31 +0100

    Initail commit

---
+++ src/com/company/HelloWord.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class HelloWord {
+    static public void main(String... args){
+        System.out.println("Projet01_Java_GetHub");
+        System.out.println("Hello word Version GitHub");
+    }
+}
